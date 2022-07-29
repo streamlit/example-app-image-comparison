c3b3f4b1d3c4df654a51c438ffdb682f937e8104
Charly Wargnier <cwar05@gmail.com>
2022-07-29 12:09:58 +0100

    Removed old set_page_config

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,8 @@ import streamlit as st
 from streamlit_image_comparison import image_comparison
 import cv2
 
-# set page config
-st.set_page_config(
-    page_title="James Webb Space Telescope vs Hubble Telescope Images","🔭")
+
+st.set_page_config("Webb Space Telescope vs Hubble Telescope", "🔭")
 
 
 st.image(
